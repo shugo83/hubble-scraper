d29477f49f56d183ca2769dc8521bb8fe2e05a86
Hugo Stephens <shugo8383@gmail.com>
2018-03-12 14:02:28 +0000

    Better error logging, reduced hardcoded waits

--- main.py
+++ main.py
@@ -1,14 +1,21 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
 import urllib.request
 from bs4 import BeautifulSoup
 import time
 import os
+import logging
 from datetime import datetime
+timeout = 10
 chrome_options = Options()
 adresses = []
 images = []
 names = []
+waiting = True
 prefs = {
     "profile.default_content_setting_values.plugins": 1,
     "profile.content_settings.plugin_whitelist.adobe-flash-player": 1,
@@ -16,12 +23,17 @@ prefs = {
     "PluginsAllowedForUrls": "https://app.hubbleconnected.com/#devices"
                              "https://app.hubbleconnected.com/#login"
 }
+format_string = '%(levelname)8s:\t%(message)s'
+logging.basicConfig(format=format_string, level=logging.INFO)
+logging.info("Logging system active")
 chrome_options.add_experimental_option("prefs", prefs)
+logging.info("Chrome experimental options set")
 
 with open('credentials.txt', 'r') as f:
     load = f.readlines()
 login = load[0]
 password = load[1]
+logging.info('Credentials Loaded')
 driver = webdriver.Chrome(chrome_options=chrome_options)
 driver.set_window_size(1920, 1080)  # ensures responsive design does not hide relevant buttons
 driver.get("https://app.hubbleconnected.com/#login")
@@ -32,16 +44,26 @@ login_button = driver.find_element_by_id("login_submit")
 username_field.send_keys(login)
 password_field.send_keys(password)
 login_button.click()
-print('logged in')
-time.sleep(5)
-# nasty way of waiting - required else ElementNotVisibleException: element not visible - cannot catch with an except
+logging.info('logged in')
+while waiting:
+    time.sleep(0.5)
+    try:
+        timeline_button = driver.find_element_by_class_name("timeline-tab")
+        timeline_button.click()
+        waiting = False
+    except Exception as E:
+        logging.error(E)
 
-timeline_button = driver.find_element_by_class_name("timeline-tab")
-timeline_button.click()
 
-time.sleep(5)
-# same as above
-print('looking')
+try:
+    element_present = EC.presence_of_element_located(
+            (By.ID, 'timeline_container'))
+    WebDriverWait(driver, timeout).until(element_present)
+    logging.info('Waiting')
+except TimeoutException:
+    logging.info('Timed out')
+
+logging.info('Looking')
 html = driver.page_source
 soup = BeautifulSoup(html, 'html.parser')
 for link in soup.find_all('a'):
@@ -50,36 +72,42 @@ for link in soup.find_all('a'):
 try:
     timeline_next = driver.find_element_by_class_name("pagelinks-last")
     count = 0
-    while count < 10:
+    while count < 10:  # Maximum number of page expected
         timeline_next.click()
-        time.sleep(5)
-        print('looking on page' + count)
+        time.sleep(5)  # not sure of an alternative
+        logging.info('looking on page' + count)
         html = driver.page_source
         soup = BeautifulSoup(html, 'html.parser')
         for link in soup.find_all('a'):
             if link.get('href') not in adresses:
                 adresses.append(link.get('href'))
 except:
-    print('No futher pages')
-    pass
-#driver.quit()
+    logging.info('No further pages found')
+driver.quit()
+
+
 script_dir = os.path.dirname(__file__)
 pos = script_dir + '/saves'
 for file in os.listdir(pos):
     k = file.split('-')[-1]
     k = k.split('.')[0]
     names.append(k)
-
+logging.info('Current images scanned')
 for i in range(len(adresses)):
     if str(adresses[i]).startswith(
             'https://hubble-resources.s3.amazonaws.com/freemium/'):
-        print('found image')
+        logging.info('Found image')
         images.append(adresses[i])
         location = adresses[i]
         dt = datetime.now().strftime('%y-%m-%d')
         idt = adresses[i].split('/')[8]
         ident = idt.split('.')[0]
         if ident not in names:
-            print('unique found')
+            logging.info('unique found')
             name = 'saves/' + str(dt) + '-' + str(ident) + '.jpg'
-            urllib.request.urlretrieve(location, name)
+            try:
+                urllib.request.urlretrieve(location, name)
+            except Exception as E:
+                logging.error(E)
+        else:
+            logging.info('Already collected')
