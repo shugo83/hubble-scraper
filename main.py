6df8f3d515e0c600133f41dd35e0b174cb9b084c
Hugo Stephens <shugo8383@gmail.com>
2018-08-08 10:18:52 +0100

    logging to file and error handling

--- main.py
+++ main.py
@@ -13,6 +13,8 @@ import sys
 from datetime import datetime
 timeout = 30
 count = 0
+image_found = 0
+image_unique = 0
 chrome_options = Options()
 adresses = []
 images = []
@@ -26,9 +28,12 @@ prefs = {
     "PluginsAllowedForUrls": "https://app.hubbleconnected.com/#devices"
                              "https://app.hubbleconnected.com/#login"
 }
-format_string = '%(levelname)8s:\t%(message)s'
-logging.basicConfig(format=format_string, level=logging.INFO)
-logging.info("Logging system active")
+format_string = '%(asctime)s - %(levelname)s - %(message)s'
+logging.basicConfig(format=format_string,
+                    filename='hubble.log',
+                    filemode='w',
+                    level=logging.INFO)
+logging.info('Logging started')
 chrome_options.add_experimental_option("prefs", prefs)
 logging.info("Chrome experimental options set")
 
@@ -43,27 +48,31 @@ password = load[1]
 logging.info('Credentials Loaded')
 driver = webdriver.Chrome(chrome_options=chrome_options)
 driver.set_window_size(1920, 1080)  # ensures responsive design does not hide relevant buttons
-driver.get("https://app.hubbleconnected.com/#login")
-username_field = driver.find_element_by_name("hubble-signin-username")
-password_field = driver.find_element_by_name("hubble-signin-password")
-login_button = driver.find_element_by_id("hubble-signin")
+driver.get('https://app.hubbleconnected.com/#login')
+username_field = driver.find_element_by_name('hubble-signin-username')
+password_field = driver.find_element_by_name('hubble-signin-password')
+login_button = driver.find_element_by_id('hubble-signin')
 
 username_field.send_keys(login)
 password_field.send_keys(password)
 login_button.click()
-logging.info('logged in')
+logging.info('Logged in')
 while waiting:
     time.sleep(2)
     try:
-        timeline_button = driver.find_element_by_class_name("hubble-dash-playwrap")
+        timeline_button = driver.find_element_by_class_name('hubble-dash-playwrap')
         timeline_button.click()
         waiting = False
     except Exception as E:
-        logging.error(E)
+        logging.warning('Waiting for load')
+        logging.debug(E)
+
         count += 2
         if count > timeout:
+            logging.critical('Timeout')
+            driver.quit()
             sys.exit(0)
-
+logging.info('Element Loading')
 try:
     element_present = EC.presence_of_element_located(
             (By.XPATH, idxpath))
@@ -72,7 +81,7 @@ try:
 except TimeoutException:
     logging.info('Timed out')
 
-logging.info('Looking')
+logging.info('Looking for images')
 html = driver.page_source
 soup = BeautifulSoup(html, 'html.parser')
 for link in soup.find_all('img'):
@@ -86,23 +95,23 @@ for file in os.listdir(pos):
     k = k.split('.')[0]
     names.append(k)
 logging.debug(adresses)
-logging.info('Current images scanned')
+logging.info('Current images in directory scanned')
 save_file_path = os.path.join(script_dir, 'saves/')
 for i in range(len(adresses)):
     if str(adresses[i]).startswith(
             'https://hubble-resources.s3.amazonaws.com/freemium/'):
-        logging.info('Found image')
+        image_found += 1
         images.append(adresses[i])
         location = adresses[i]
         dt = datetime.now().strftime('%y-%m-%d')
         idt = adresses[i].split('/')[8]
         ident = idt.split('.')[0]
         if ident not in names:
-            logging.info('unique found')
+            image_unique += 1
             name = save_file_path + str(dt) + '-' + str(ident) + '.jpg'
             try:
                 urllib.request.urlretrieve(location, name)
             except Exception as E:
                 logging.error(E)
-        else:
-            logging.info('Already collected')
+logging.info(
+        str(image_found) + ' images found, ' + str(image_unique) + ' new')
